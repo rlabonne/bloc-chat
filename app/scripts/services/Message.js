ce18f07e7916c6b8c541e50793f76660ebe85732
Rob LaBonne <roblabonne@gmail.com>
2017-06-28 01:42:44 -0400

    6 send messages complete

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -1,5 +1,5 @@
 (function() {
-  function Message($firebaseArray) {
+  function Message($firebaseArray, $cookies, Room) {
     var ref = firebase.database().ref().child("messages");
 
     var messages = $firebaseArray(ref);
@@ -10,11 +10,22 @@
         // Filter the messages by their room ID.
 
         return $firebaseArray(ref.orderByChild('roomId').equalTo(roomId));
+      },
+
+      send: function(newMessage, roomId) {
+        var messageDateTime = new Date;
+        messages.$add({
+          username: $cookies.get('blocChatCurrentUser'),
+          content: newMessage,
+          roomId: roomId,
+          sentAt: messageDateTime.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
+        });
+
       }
     };
   }
 
   angular
     .module('blocChat')
-    .factory('Message', ['$firebaseArray', Message]);
+    .factory('Message', ['$firebaseArray', '$cookies', 'Room', Message]);
 })();
